f63c78d3ead705a9b3e9908f8c2a08f25bf24f0a
justine-adan <justine.paul.adan.06@gmail.com>
2026-04-18 14:05:04 +0800

    refactor header for better design

--- MyApplication/clientapp/src/components/header/header.tsx
+++ MyApplication/clientapp/src/components/header/header.tsx
@@ -1,31 +1,48 @@
 import { useState } from "react";
-import { Box, Flex, HStack, Link, Image, Button, Modal, ModalOverlay, ModalContent, ModalHeader, ModalBody, ModalCloseButton, Menu, MenuButton, MenuList, MenuItem, Avatar } from "@chakra-ui/react";
+import { Box, Flex, HStack, Link, Text, Button, Modal, ModalOverlay, ModalContent, ModalHeader, ModalBody, ModalCloseButton, Menu, MenuButton, MenuList, MenuItem, Avatar, Badge, Divider, Icon } from "@chakra-ui/react";
 import s from "./header.module.scss";
 import { Login } from "../logins/login";
 import { SignUp } from "../logins/signUp";
 import { useAuth } from "../../context/authContext";
+import { useNavigate } from "react-router-dom";
+import { FiClock, FiLock, FiLogOut, FiUser, FiUsers } from "react-icons/fi";
+import { RxDashboard } from "react-icons/rx";
+import { LuBanknote } from "react-icons/lu";
+import { BsGraphUpArrow } from "react-icons/bs";
+import { GoGear } from "react-icons/go";
 
 const Header = () => {
   const { user, logout } = useAuth();
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const [isRegisterOpen, setIsRegisterOpen] = useState<boolean>(false);
+  const navigate = useNavigate();
+
   const navByRole = (role: string) => {
     switch (role) {
       case "Admin":
-        return [{ label: "Dashboard", href: "/admin/dashboard" }];
+        return [
+          {
+            label: "Dashboard", icon: <RxDashboard />
+            , href: "/admin/dashboard"
+          },
+          { label: "Users", icon: <FiUsers  />, href: "/admin/users" },
+          { label: "Accounts", icon: <LuBanknote />, href: "/admin/accounts" },
+          { label: "Reports", icon: <BsGraphUpArrow  />, href: "/admin/settings" },
+        ];
 
       case "Teller":
         return [
-          { label: "Dashboard", href: "/teller/dashboard" },
-          { label: "Customers", href: "/teller/customers" },
+          { label: "Dashboard", icon: <RxDashboard />, href: "/teller/dashboard" },
+          { label: "Customers", icon: <FiUsers  />, href: "/teller/customers" },
+          { label: "Reports", icon: <BsGraphUpArrow  />, href: "/admin/settings" },
+          { label: "Accounts", icon: <LuBanknote />, href: "/admin/accounts" },
         ];
 
       case "User":
         return [
-          { label: "Dashboard", href: "/dashboard" },
-          { label: "Transactions", href: "/transactions" },
-          { label: "Accounts", href: "/accounts" },
-          { label: "Settings", href: "/settings" },
+          { label: "Dashboard", icon: <RxDashboard />, href: "/dashboard" },
+          { label: "Transactions", icon: <FiClock />, href: "/transactions" },
+          { label: "Settings", icon: <GoGear  />, href: "/settings" },
         ];
       default:
         return [];
@@ -50,22 +67,22 @@ const Header = () => {
   return (
     <Box className={s.header}>
       <Flex align="center" justify="space-between" w="100%">
-        <Box className={s.logo}>
-          <Link href="/">
-            <Image src="/logo.png" alt="Logo" />
-          </Link>
-        </Box>
+        <Link className={s.logo} href="/" _hover={{ textDecoration: "none" }}>
+          <img src="./logo1.png" alt="Bankify Logo" />
+          <span>Bankify</span>
+        </Link>
 
-        <HStack className={s.navLinks}>
+        <HStack spacing={8}>
           {user &&
             navItems.map((nav) => (
-              <Link key={nav.label} href={nav.href} fontWeight="bold">
-                {nav.label}
+              <Link className={s.navLinks} key={nav.label} href={nav.href} fontWeight="bold" _hover={{ textDecoration: "none" }}>
+               {nav.icon}
+               {nav.label}
               </Link>
             ))}
         </HStack>
 
-        <Flex className={s.actionButtons} align="center">
+        <Flex className={s.actionButtons} align="center" >
           {!user ? (
             <>
               <Button onClick={() => setIsOpen(true)}>Login</Button>
@@ -81,14 +98,44 @@ const Header = () => {
                 />
               </MenuButton>
 
-              <MenuList>
-                <MenuItem>{user.email}</MenuItem>
+              <MenuList p={3} borderRadius="md" boxShadow="lg">
+                <Flex align="center" mb={3}>
+                  <Avatar
+                    size="md"
+                    name={user.email}
+                    getInitials={(name) => getInitials(name)}
+                  />
+                  <Box flex={1} ml={4}>
+                    <Text fontWeight="bold">{user.email}</Text>
+                    <Badge colorScheme="green" borderRadius="full" px={2}>
+                      User
+                    </Badge>
+                  </Box>
 
-                <MenuItem color={getRoleColor(user.role)}>
-                  Role: {user.role}
+                </Flex>
+                <Divider mb={2} />
+
+                <MenuItem icon={<Icon as={FiUser} />}>
+                  Profile Settings
                 </MenuItem>
 
-                <MenuItem onClick={() => logout(false)} color="red.500">
+                <MenuItem icon={<Icon as={FiLock} />}>
+                  Security (PIN / Password)
+                </MenuItem>
+
+                <MenuItem icon={<Icon as={FiClock} />}>
+                  Activity Log
+                </MenuItem>
+
+                <Divider my={2} />
+
+                {/* Logout */}
+                <MenuItem
+                  icon={<Icon as={FiLogOut} />}
+                  color="red.500"
+                  _hover={{ bg: "red.50" }}
+                  onClick={() => logout(false)}
+                >
                   Logout
                 </MenuItem>
               </MenuList>
@@ -96,8 +143,6 @@ const Header = () => {
           )}
         </Flex>
       </Flex>
-
-      {/* Login Modal */}
       <Modal isOpen={isOpen} onClose={() => setIsOpen(false)} isCentered>
         <ModalOverlay />
         <ModalContent>
@@ -105,22 +150,6 @@ const Header = () => {
           <ModalCloseButton />
           <ModalBody pb={6}>
             <Login onSuccess={() => setIsOpen(false)} />
-          </ModalBody>
-        </ModalContent>
-      </Modal>
-
-      {/* Register Modal */}
-      <Modal
-        isOpen={isRegisterOpen}
-        onClose={() => setIsRegisterOpen(false)}
-        isCentered
-      >
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Register</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody pb={6}>
-            <SignUp onSuccess={() => setIsRegisterOpen(false)} />
           </ModalBody>
         </ModalContent>
       </Modal>
